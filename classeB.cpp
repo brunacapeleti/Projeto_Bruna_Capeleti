4bb321b3ac2fe1b71de4d6228ed32b76076a0634
Bruna Capeleti <63613165+brunacapeleti@users.noreply.github.com>
2020-04-15 17:24:24 -0300

    Update classeB.cpp

--- classeB.cpp
+++ classeB.cpp
@@ -3,12 +3,12 @@ class B{
       float B1;
       int B2;
    public:
-      getB1();
-      setB1(float valor1);
-      getB2();
-      setB2(int valor2);
-      MB1();
-      MB2();
+      float getB1();
+      void setB1(float valor1);
+      int getB2();
+      void setB2(int valor2);
+      void MB1();
+      void MB2();
 };
 
 B::B(){
@@ -20,7 +20,7 @@ float B::getB1(){
   return B1;
 }
 
-float B::setB1(float valor1){
+void B::setB1(float valor1){
   B1 = valor1;
 }
 
@@ -28,14 +28,14 @@ int B::getB2(){
   return B2;
 }
 
-int B::setB2(int valor2){
+void B::setB2(int valor2){
   B2 = valor2;
 }
 
-float B::MB1(){
+void B::MB1(){
   cout << B1 << endl;
 }
 
-int B::MB2(){
+void B::MB2(){
   cout << B2 << endl;
 }
