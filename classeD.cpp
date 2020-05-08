a4261459a3e359bac38e636bbfc2746f428dad1c
Bruna Capeleti <63613165+brunacapeleti@users.noreply.github.com>
2020-05-08 15:41:12 -0300

    Update classeD.cpp

--- classeD.cpp
+++ classeD.cpp
@@ -6,7 +6,7 @@ class D{
     D();
     void MD1();
     void MD2();
-    void MD4();
+    void MD3();
 };
 
 D::D(){
@@ -22,8 +22,11 @@ void D::MD2(){
     cout << "MD2" << endl;
 }
 
-void D::MD4(){
-    cout << "MD4" << endl;
+void D::MD3(){
+    cout << "MD3" << endl;
 }
+
+
+
 
 
